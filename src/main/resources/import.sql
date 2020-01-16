e90fafb4a36ce7b084192c5a56d3daf89382fd75
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-01-16 11:52:56 -0500

    add properties config postgreSql and dependency config client

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('miguel', '$2a$10$j2idwqfNasMa6Uw65ahxzO8.YgKA2DkZKz2odvhfmbcPOvIQ4AqzW', 1, 'Miguel','Chinchay', 'miguel@tuna.pe');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$jIfPlHBgdmD99JAiWTYiLOzjjooXuVN2edwhVMoe0kpJ3aFm9cS7G', 1, 'Admin','Xinxay', 'admin@tuna.pe');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('miguel', '$2a$10$j2idwqfNasMa6Uw65ahxzO8.YgKA2DkZKz2odvhfmbcPOvIQ4AqzW', true, 'Miguel','Chinchay', 'miguel@tuna.pe');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$jIfPlHBgdmD99JAiWTYiLOzjjooXuVN2edwhVMoe0kpJ3aFm9cS7G', true, 'Admin','Xinxay', 'admin@tuna.pe');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
